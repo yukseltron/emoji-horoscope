e9dbcfa12d45670a6b65334459fc5b730f8d1dac
hamid.yuksel <h.yuksel04@gmail.com>
2019-01-08 16:55:34 -0500

    implement IBM watson tone analysis API"

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
+import json
 import requests
+from watson_developer_cloud import ToneAnalyzerV3
+
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    iam_apikey='xakWAE0E92YVWWnzf5rYgOLKZcJb5kecM_zINqqLKZCY',
+    url='https://gateway.watsonplatform.net/tone-analyzer/api'
+)
 
 params = (
-('sign', 'aries'),
+('sign', 'taurus'),
 ('day', 'today'),
 )
 
@@ -12,3 +20,9 @@ print(r.status_code == requests.codes.ok)
 
 data = r.json()
 print(data['description'])
+
+tone_analysis = tone_analyzer.tone(
+    {'text': data['description']},
+    'application/json'
+).get_result()
+print(json.dumps(tone_analysis[, indent=2))
