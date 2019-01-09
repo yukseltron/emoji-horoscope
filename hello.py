64dc1789342832c3f67e50b93397f6f8ba2ed91e
hamid.yuksel <h.yuksel04@gmail.com>
2019-01-08 22:21:20 -0500

    refactor code for better organization before it is too late

--- hello.py
+++ hello.py
@@ -2,27 +2,41 @@ import json
 import requests
 from watson_developer_cloud import ToneAnalyzerV3
 
-tone_analyzer = ToneAnalyzerV3(
-    version='2017-09-21',
-    iam_apikey='xakWAE0E92YVWWnzf5rYgOLKZcJb5kecM_zINqqLKZCY',
-    url='https://gateway.watsonplatform.net/tone-analyzer/api'
-)
 
 params = (
 ('sign', 'taurus'),
 ('day', 'today'),
 )
 
-r = requests.post('https://aztro.sameerkumar.website/', params=params)
 
-print(r.status_code)
-print(r.status_code == requests.codes.ok)
+def getScope(params):
+    r = requests.post('https://aztro.sameerkumar.website/', params=params)
+    data = r.json()
 
-data = r.json()
-print(data['description'])
+    return data
 
-tone_analysis = tone_analyzer.tone(
-    {'text': data['description']},
-    'application/json'
-).get_result()
-print(json.dumps(tone_analysis[, indent=2))
+
+def getTone(data):
+    tone_analyzer = ToneAnalyzerV3(
+        version='2017-09-21',
+        iam_apikey='xakWAE0E92YVWWnzf5rYgOLKZcJb5kecM_zINqqLKZCY',
+        url='https://gateway.watsonplatform.net/tone-analyzer/api'
+    )
+
+    tone_analysis = tone_analyzer.tone(
+        {'text': data},
+        'application/json'
+    ).get_result()
+
+    return tone_analysis
+
+
+def analyzeScope():
+    data = getScope(params)
+    tone = getTone(data['description'])
+    emotionalTone = tone['document_tone']['tones'][0]['tone_id']
+    languageTone = tone['document_tone']['tones'][1]['tone_id']
+
+    print(languageTone)
+
+analyzeScope()
